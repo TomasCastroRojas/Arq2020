1f6412e952f7c4b9186230752676b4c87010527c
TomasCastro <tomascastrorojas18@gmail.com>
2020-10-20 18:10:28 -0300

    ejercicio 6 plancha 2

---
+++ Plancha2/ejercicio6/ej6.s
@@ -0,0 +1,51 @@
+.data
+format: .asciz "%ld\n"
+formatC: .asciz "0x%08X\n"
+i:      .quad 0xDEADBEEF
+.text
+.global main
+main:
+
+  #Apartado a
+  movq $format, %rdi
+  movq %rsp, %rsi
+  xorq %rax, %rax
+  call printf
+
+  #Apartado b
+  movq $format, %rdi
+  leaq format, %rsi
+  xorq %rax, %rax
+  call printf
+
+  #Apartado c
+  movq $formatC, %rdi
+  leaq formatC, %rsi
+  xorq %rax, %rax
+  call printf
+
+  #Apartado d
+  movq $format, %rdi
+  movq (%rsp), %rsi
+  xorq %rax, %rax
+  call printf
+
+  #Apartado e
+  movq $format, %rdi
+  movq 8(%rsp), %rsi
+  xorq %rax, %rax
+  call printf
+
+  #Apartado f
+  movq $formatC, %rdi
+  movq i, %rsi
+  xorq %rax, %rax
+  call printf
+
+  #Apartado g
+  movq $format, %rdi
+  leaq i, %rsi
+  xorq %rax, %rax
+  call printf
+
+  ret
