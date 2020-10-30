e936ce537bec20c5086078246732ca15a5430ccf
Blas Barbagelata <blasbarbagelata@gmail.com>
2020-10-29 21:05:29 -0300

    aguante boca

---
+++ Plancha2/ejercicio5/ejercicio5.s
@@ -0,0 +1,30 @@
+.global fact2
+
+
+fact2:
+    movq $1 , %rax
+    movq $0 , %rcx
+    movq %rdi , %rcx
+    bucle:
+        imulq %rcx, %rax
+        loop bucle
+    ret
+   
+.global fact1
+fact1:
+    pushq %rbp
+    movq %rsp, %rbp
+    cmpq $1, %rdi
+    jz casobase
+    pushq %rdi
+    decq %rdi
+    call fact1
+    popq %rdi
+    mulq %rdi
+    terminopadre:
+        movq %rbp, %rsp
+        popq %rbp
+    ret
+    casobase:
+        movq %rdi, %rax
+        jmp terminopadre 
