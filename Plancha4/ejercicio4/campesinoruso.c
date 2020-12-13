199fca464f6edc22c9c073e4e1cf31f075bc27be
TomasCastro <tomascastrorojas18@gmail.com>
2020-12-13 16:21:49 -0300

    comparacion entre implementacion de ARM y C

--- Plancha4/ejercicio4/campesinoruso.c
+++ Plancha4/ejercicio4/campesinoruso.c
@@ -5,30 +5,26 @@
 
 unsigned int campesino_ruso(unsigned int i, unsigned int j);
 
-/*unsigned int campesino_ruso(unsigned int i, unsigned int j) {
+unsigned int campesino_rusoC(unsigned int i, unsigned int j) {
   unsigned int res = 0;
-  unsigned iter = 0;
   while (j > 1) {
     if (j & 1) {
       res += i;
       j -= 1;
-      printf ("iter %d -- res = %d -- j = %d\n", iter, res, j);
-      iter++;
     } else {
       i *= 2;
       j /= 2;
-      printf ("iter %d -- i = %d -- j = %d\n", iter, i, j);
-      iter++;
     }
   }
   return res + i;
-}*/
+}
 
 int main (int argc, char* argv[]) {
   if (argc == 3){
     unsigned int a = atoi(argv[1]);
     unsigned int b = atoi(argv[2]);
-    printf ("El resultado es: %d\n", campesino_ruso(a, b));
+    printf ("El resultado en la implementacion de ARM: %u\n", campesino_ruso (a, b));
+    printf ("El resultado en la implementacion de C: %u\n", campesino_rusoC (a, b));
   }
-  else printf ("No se ingreso la cantidad correcta de argumentos");
+  else printf ("No se ingreso la cantidad correcta de argumentos\n");
 }
