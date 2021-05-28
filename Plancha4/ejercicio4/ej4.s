74e4f8ec330aba605ec86b21d0da19757745e6d8
TomasCastro <tomascastrorojas18@gmail.com>
2021-05-28 14:34:22 -0300

    Arreglado caso que el segundo argumento era 1. Ademas makefile para compilar

--- Plancha4/ejercicio4/ej4.s
+++ Plancha4/ejercicio4/ej4.s
@@ -2,12 +2,14 @@
 campesino_ruso:
   EOR r2, r2, r2     @ r2 = res = 0
   bucle:
-  ANDS r3, r1, #1    @ j & 1
-  ADDNE r2, r2, r0   @ res += i
-  SUBNE r1, r1, #1   @ j -= 1;
-  LSLEQ r0, #1       @ i += 2;
-  LSREQ r1, #1       @ j /= 2;
-  CMP r1, #1
-  BHI bucle @ jump if j > 1
-  ADD r0, r0, r2     @ i += res
-  Bx lr
+    CMP r1, #1
+    BLS fin
+    ANDS r3, r1, #1    @ j & 1
+    ADDNE r2, r2, r0   @ res += i
+    SUBNE r1, r1, #1   @ j -= 1;
+    LSLEQ r0, #1       @ i += 2;
+    LSREQ r1, #1       @ j /= 2;
+    B bucle
+  fin:
+    ADD r0, r0, r2     @ i += res
+    Bx lr
